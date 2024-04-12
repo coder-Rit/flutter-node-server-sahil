266f82fc9ace707a20bb60ed79a1f70bc48640e8
coder-Rit <001patilritesh@gmail.com>
2024-04-13 02:24:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const error = require('./middleware/error')
+const connectTODatabase = require('./config/dataBase')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const cors  = require("cors")
+const app = express()
+
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "config/config.env" });
+  }
+ 
+app.use(cors({
+  credentials: true,
+  origin:[process.env.ORIGIN_1,process.env.ORIGIN_2,process.env.ORIGIN_]
+  
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended:true}))
+
+ 
+
+//connection to the data base 
+connectTODatabase()
+
+
+ 
+
+
+const userRouter = require('./router/userRouter') 
+const productInfoRouter = require('./router/productInfoRouter') 
+const uploadsRouter = require('./router/uploadsRouter') 
+ 
+
+app.use("/api/v1", userRouter)
+app.use("/api/v1", productInfoRouter)
+app.use("/api/v1", uploadsRouter)
+ 
+
+ 
+ 
+ 
+ app.use(error)
+module.exports = app
